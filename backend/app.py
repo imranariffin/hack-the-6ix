e1a6cc71189b60d48a4ebcfdd0ef292b0b813923
Imran Ariffin <ariffinimran@gmail.com>
2017-08-26 11:46:38 -0400

    gitignore env/, python, node

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,17 @@
+from livereload import Server
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
 
+@app.route('/users')
+def get_users():
+	return 'users'
+
 if __name__ == '__main__':
-	app.run(port=8080)
+	# app.run(port=8080)
+	server = Server(app)
+	server.serve(port=8080)
