2389cf5600b28bfe79234c45c543564fcbb1f114
Imran Ariffin <ariffinimran@gmail.com>
2017-08-27 02:28:25 -0400

    thankyou page redirects to root if no user

--- frontend/src/components/basecomponents/ThankYou.js
+++ frontend/src/components/basecomponents/ThankYou.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-// import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import URLS from '../../constants/urls';
 
 const volunteerThankyouMsg = "Thank you for joining us as vounteers";
 const seniorThankyouMsg = "Thank you for choosing to use our service. We'll provide you with volunteers available to be at service";
 
 class ThankYou extends Component {
 	render() {
+		if (!this.props.authenticated()) {
+			return <Redirect to={URLS.ROOT} />
+		}
+
 		console.log('ThankYou.render()');
 		const thankyouMsg = this.props.isV()
 			? volunteerThankyouMsg
