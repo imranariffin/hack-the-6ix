5f44527cbbaec84cd43163028bd43b0581cf990a
Vikram Grover <vikgrover1@gmail.com>
2017-08-27 05:02:54 -0400

    added more backend functions.

--- backend/app.py
+++ backend/app.py
@@ -45,25 +45,93 @@ def validate_login(email_id,password):
     return json_data
 
 @app.route('/v/signup', methods = ['POST'])
-def v_signup(first_name,last_name,email,password,phone_number,address,languages,description,skills):
+def v_signup(first_name,last_name,email,password,phone_number,address,languages,description,cooking,cleaning,conversations,groceries):
     global c
     c.execute('select 0 from all_users where email = ''?''',email)
     email_exists = c.fetchall()
     if email_exists == 0:
             status_dictionary = {"Status" : 400,
              "user" : {"email" : email_id,
-                       "user_type" : user_type},
+                       "user_type" : 'V'},
              "token" : "discuss API"}
     else:
-        c.execute('insert into voulnteer_info (first_name,last_name,email,password,phone_number,address,languages,description,skills) values(''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?''',
-        first_name,last_name,email,password,phone_number,address,languages,description,skills)
-        c.execute('insert into all_users (first_name,last_name,email,password,phone_number,address,languages,description,skills,user_type) values(''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?''',
-        first_name,last_name,email,password,phone_number,address,languages,description,skills,'V')
+        c.execute('insert into voulnteer_info (first_name,last_name,email,password,phone_number,address,languages,description) values(''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?''',
+        first_name,last_name,email,password,phone_number,address,languages,description)
+        c.execute('insert into skills (email,cooking,cleaning,conversations,groceries) values (''?'',''?'',''?'',''?'',''?''',cooking,cleaning,conversations,groceries)
+        c.execute('insert into all_users (first_name,last_name,email,password,phone_number,address,languages,description,skills,user_type) values(''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?''',
+        first_name,last_name,email,password,phone_number,address,languages,description,'V')
         status_dictionary = {"Status" : 200,
                                  "user" : {"email" : email_id,
-                                           "user_type" : user_type},
+                                           "user_type" : 'V'},
                                  "token" : "discuss API"}
     json_data = json.dumps(status_dictionary, ensure_ascii=False)
+    return json_data
+
+@app.route('/s/signup', methods = ['POST'])
+def v_signup(first_name,last_name,email,password,phone_number,address,languages,cooking,cleaning,conversations,groceries):
+    global c
+    c.execute('select 0 from all_users where email = ''?''',email)
+    email_exists = c.fetchall()
+    if email_exists == 0:
+            status_dictionary = {"Status" : 400,
+             "user" : {"email" : email_id,
+                       "user_type" : 'S'},
+             "token" : "discuss API"}
+    else:
+        c.execute('insert into senior_info (first_name,last_name,email,password,phone_number,address,languages) values(''?'',''?'',''?'',''?'',''?'',''?'',''?''',
+        first_name,last_name,email,password,phone_number,address,languages)
+        c.execute('insert into skills (email,cooking,cleaning,conversations,groceries) values (''?'',''?'',''?'',''?'',''?''',cooking,cleaning,conversations,groceries)
+        c.execute('insert into all_users (first_name,last_name,email,password,phone_number,address,languages,user_type) values(''?'',''?'',''?'',''?'',''?'',''?'',''?'',''?''',
+        first_name,last_name,email,password,phone_number,address,languages,'S')
+        status_dictionary = {"Status" : 200,
+                                 "user" : {"email" : email_id,
+                                           "user_type" : 'S'},
+                                 "token" : "discuss API"}
+    json_data = json.dumps(status_dictionary, ensure_ascii=False)
+    return json_data
+
+@app.route('/s/volunteers', methods = ['GET'])
+def seniors_volunteers(email,token):
+    global c
+    skill_set = null
+    c.execute('select cooking from skills where email = ''?''',email)
+    cooking = c.fetchall()
+    if cooking is not null:
+        skill_set = '(''Cooking'''
+
+    c.execute('select cleaning from skills where email = ''?''',email)
+    cleaning = c.fetchall()
+    if cleaning is not null and skill_set is not null:
+        skill_set = skill_set + ',''Cleaning'''
+    elif cleaning is not null and skill_set is null:
+        skill_set = '(''Cleaning'''
+
+    c.execute('select conversations from skills where email = ''?''',email)
+    conversations = c.fetchall()
+    if conversations is not null and skill_set is not null:
+        skill_set = skill_set + ',''Conversations'''
+    elif cleaning is not null and skill_set is null:
+        skill_set = '(''Conversations'''
+
+    c.execute('select groceries from skills where email = ''?''',email)
+    groceries = c.fetchall()
+    if groceries is not null and skill_set is not null:
+        skill_set = skill_set + ',''Groceries'''
+    elif groceries is not null and skill_set is null:
+        skill_set = '(''Groceries'''
+
+    if skill_set is not null:
+        skill_set = skill_set + ')'
+
+    for row in c.execute('select email_id from skills where (cooking in ? or conversations in ? or groceries in ? or cleaning in ?) and user_type = ''V''',skill_set,skill_set,skill_set,skill_set):
+        c.execute('insert into best_matches(senior_email,volunteers_email) values(''?'',''?'')',email,row[0])
+    name = c.fetchall()
+    if name is not null:
+            status_dictionary = {"Status" : 200,
+                                 "volunteers" : [{"email" : email,
+                                                  "name" : name,
+                                                  "services_provided" : 0}
+                                 ]}
     return json_data
 
 if __name__ == '__main__':
--- backend/create_tables.py
+++ backend/create_tables.py
@@ -9,7 +9,7 @@ c.execute('''CREATE TABLE senior_info
 
 # Table to store volunteer_info.
 c.execute('''CREATE TABLE voulnteer_info
-             (first_name,last_name,email,password,phone_number,address,languages,description,skills)''')
+             (first_name,last_name,email,password,phone_number,address,languages,description)''')
 
 # Table to make sure login was successful irrespective of the user type.
 c.execute('''CREATE TABLE active_sessions
@@ -18,6 +18,22 @@ c.execute('''CREATE TABLE active_sessions
 # All users info table.
 c.execute('''CREATE TABLE all_users
              (first_name,last_name,email,password,phone_number,address,languages,description,skills,user_type)''')
+
+# Skills data table.
+c.execute('''CREATE TABLE skills
+             (email,user_type,cooking,cleaning,conversations,groceries)''')
+
+# Volunteer that matches with the senior.
+c.execute('''CREATE TABLE best_matches
+             (senior_email,volunteers_email)''')
+
+# Offers made by senior.
+c.execute('''CREATE TABLE offers_made
+             (senior_email,volunteers_email)''')
+
+# Offers received by volunteers.
+c.execute('''CREATE TABLE offers_received
+             (volunteers_email,senior_email)''')
 
 conn.commit()
 conn.close()
